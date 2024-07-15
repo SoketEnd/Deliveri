584808779608b1d5cd2e23e414b3f369ae9d5a55
Name <email@example.ru>
2024-07-15 16:09:56 +0300

    Добавьте файлы проекта.

---
+++ Deliveri/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliveri
+{
+    abstract class Delivery
+    {
+        public abstract string Address { get; set; }
+    }
+
+    class HomeDelivery : Delivery
+    {
+        private string address;
+
+        private List<string> products;
+        public override string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public List<string> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+        public HomeDelivery(string address, List<string> products)
+        {
+            this.address = address;
+            this.products = products;
+        }
+    }
+    class PickPointDelivery : Delivery
+    {
+        private string address;
+        private List<string> products;
+        public override string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public List<string> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+        public PickPointDelivery(string address, List<string> products)
+        {
+            this.address = address;
+            this.products = products;
+        }
+    }
+    class RestaurantDelivery : Delivery
+    {
+        private string address;
+        private List<string> products;
+        public override string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public List<string> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+        public RestaurantDelivery(string address, List<string> products)
+        {
+            this.address = address;
+            this.products = products;
+        }
+    }
+    class ShopDelivery : Delivery
+    {
+        private string address;
+        private List<string> products;
+        public override string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public List<string> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+        public ShopDelivery(string address, List<string> products)
+        {
+            this.address = address;
+            this.products = products;
+        } 
+    }
+    class GetProducts
+    {
+        public List<string> ChooseProducts()
+        {
+            List<string> products = new List<string>();
+            Console.WriteLine("Выберите продукты (введите 'end' для завершения):");
+
+            while (true)
+            {
+                string product = Console.ReadLine();
+                if (product.ToLower() == "end")
+                {
+                    break;
+                }
+                products.Add(product);
+            }
+            return products;
+        }
+    }
+    class GetAddress
+    {
+        public Delivery GetAdd(string Deliverytype, List<string> products = null)
+        {
+            string address;
+            
+            if (Deliverytype == "Дом")
+            {
+                Console.WriteLine("Введите адрес доставки:");
+                address = Console.ReadLine();
+
+                return new HomeDelivery(address, products);
+            }
+            else if (Deliverytype == "Пункт")
+            {
+                Console.WriteLine("Введите адрес пункта выдачи:");
+                address = Console.ReadLine();
+                return new PickPointDelivery(address, products);
+            }else if(Deliverytype == "Самовывоз")
+            {
+                Console.WriteLine("Введите адрес Ресторана :");
+                address = Console.ReadLine();
+                return new RestaurantDelivery(address, products);
+            }
+            else
+            {
+                throw new ArgumentException("Такого типа доставки нет");
+            }
+                
+        }
+    }
+    
+    class Display 
+    {
+        public void Show()
+        {
+            GetProducts getProducts = new GetProducts();
+            List<string> products = getProducts.ChooseProducts();
+
+            GetAddress getAddress = new GetAddress();
+
+            Console.WriteLine("Введите тип доставки (Дом/Пункт/Самовывоз):");
+            string deliveryType = Console.ReadLine();
+
+            Delivery delivery = getAddress.GetAdd(deliveryType, products);
+            Console.WriteLine("Адрес доставки: " + delivery.Address);
+
+            if (delivery is HomeDelivery shopDelivery)
+            {
+                Console.WriteLine("Выбранные продукты:");
+                foreach (var product in shopDelivery.Products)
+                {
+                    Console.WriteLine("- " + product);
+                }
+            }
+            else if (delivery is PickPointDelivery shopDeliver)
+            {
+                Console.WriteLine("Выбранные продукты:");
+                foreach (var product in shopDeliver.Products)
+                {
+                    Console.WriteLine("- " + product);
+                }
+            }
+            else if (delivery is RestaurantDelivery shopDeliveri)
+            {
+                Console.WriteLine("Выбранные продукты:");
+                foreach (var product in shopDeliveri.Products)
+                {
+                    Console.WriteLine("- " + product);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Display display = new Display();
+            display.Show();
+
+        }
+    }
+}
---
+++ Deliveri/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Deliveri")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Deliveri")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("de44e088-7ef3-45cb-8979-030fb8f8e0c5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
